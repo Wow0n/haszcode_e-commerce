fe7e8f4f2ea5e3d0e520883003f316b3b679742a
Mateusz Luczak <mati.maxx@o2.pl>
2021-04-06 14:03:17 +0200

    changed http methods

--- app.py
+++ app.py
@@ -7,20 +7,20 @@ import models
 app = FastAPI(openapi_tags=tags.tags_metadata)
 
 
-@app.post("/warehouse/select/", status_code=200, tags=["Warehouse"])
+@app.get("/warehouse/select", status_code=200, tags=["Warehouse"])
 def warehouse_storage():
     return queries.warehouse_select()
 
 
-@app.post("/warehouse/insert/", status_code=200, tags=["Warehouse"])
+@app.post("/warehouse/insert", status_code=201, tags=["Warehouse"])
 def warehouse_insert(item: models.Item):
     return queries.warehouse_insert(item)
 
 
-@app.post("/warehouse/delete/", status_code=200, tags=["Warehouse"])
+@app.delete("/warehouse/delete", status_code=200, tags=["Warehouse"])
 def warehouse_delete(id: int):
     return queries.warehouse_delete(id)
 
-@app.post("/client/select/", status_code=200, tags=["Client"])
+@app.get("/client/select", status_code=200, tags=["Client"])
 def client_product_list():
     return queries.client_select()
